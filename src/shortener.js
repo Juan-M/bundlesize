e2269bb0b1337a4e319af5c380965668b8d23b6f
Juan M. Garcia Gonzalez <juan.garcia@intellisys.com.do>
2020-04-20 16:06:16 +0200

    Change shortening service

--- src/shortener.js
+++ src/shortener.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const qs = require('querystring')
 
 const googleApiKey = 'AIzaSyACIqikanS3eoHsy14JYyFAHU1aU5w1YoA'
 const url = 'https://www.googleapis.com/urlshortener/v1/url'
@@ -6,10 +7,9 @@ const url = 'https://www.googleapis.com/urlshortener/v1/url'
 const shorten = longUrl =>
   axios({
     method: 'POST',
-    url: `${url}?key=${googleApiKey}`,
-    data: {
-      longUrl
-    }
+    url: 'https://tinyurl.com/api-create.php',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify({ url: longUrl })
   })
 
 const shortener = { shorten }
